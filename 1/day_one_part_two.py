5368add92934e0abdb8e47618d63dc1654c24a95
Andrew Almaguer <andrew.almaguer@jambit.com>
2022-12-05 08:08:26 +0100

    Update previous days

--- 1/day_one_part_two.py
+++ 1/day_one_part_two.py
@@ -1,28 +1,32 @@
-def carriedCalories(elf):
+'''Advent of Code 2022 Day One Part Two'''
+
+def carried_calories(elf):
+    '''helper method that take elf pair and returns carried calories'''
     return elf[0]
 
 
 def main():
-    input = open('input.txt', 'r')
-    lines = input.readlines()
+    '''https://adventofcode.com/2022/day/1#part2'''
+    with open('input.txt', 'r', encoding="utf8") as input_file:
+        lines = input_file.readlines()
 
-    current_elf = (0, 1)
-    top_three_elves = []
+        current_elf = (0, 1)
+        top_three_elves = []
 
-    for line in lines:
-        if line.strip() == '':
-            top_three_elves.append(current_elf)
-            if len(top_three_elves) > 3:
-                top_three_elves.sort(reverse=True, key=carriedCalories)
-                top_three_elves.pop(3)
-            current_elf = (0, current_elf[1] + 1)
-        else:
-            current_elf = (current_elf[0] + int(line), current_elf[1])
+        for line in lines:
+            if line.strip() == '':
+                top_three_elves.append(current_elf)
+                if len(top_three_elves) > 3:
+                    top_three_elves.sort(reverse=True, key=carried_calories)
+                    top_three_elves.pop(3)
+                current_elf = (0, current_elf[1] + 1)
+            else:
+                current_elf = (current_elf[0] + int(line), current_elf[1])
 
-    top_three_elves_calories = (top_three_elves[0][0] + top_three_elves[1][0] +
-                                top_three_elves[2][0])
+        top_three_elves_calories = (top_three_elves[0][0] + top_three_elves[1][0] +
+                                    top_three_elves[2][0])
 
-    print(f'The top 3 elves are carrying {top_three_elves_calories} calories')
+        print(f'The top 3 elves are carrying {top_three_elves_calories} calories')
 
 
 if __name__ == '__main__':
--- 2/day_two_part_one.py
+++ 2/day_two_part_one.py
@@ -1,36 +1,39 @@
+'''Advent of Code 2022 Day Two'''
+
 def main():
-    input = open('input.txt', 'r')
-    lines = input.readlines()
+    '''https://adventofcode.com/2022/day/2'''
+    with open('input.txt', 'r', encoding="utf8") as input_file:
+        lines = input_file.readlines()
 
-    score = 0
+        score = 0
 
-    for line in lines:
-        opponent = line[0]
-        user = line[2]
-        match user:
-            case 'X':
-                score += 1
-                if opponent == 'A':
+        for line in lines:
+            opponent = line[0]
+            user = line[2]
+            match user:
+                case 'X':
+                    score += 1
+                    if opponent == 'A':
+                        score += 3
+                    elif opponent == 'C':
+                        score += 6
+                case 'Y':
+                    score += 2
+                    if opponent == 'B':
+                        score += 3
+                    elif opponent == 'A':
+                        score += 6
+                case 'Z':
                     score += 3
-                elif opponent == 'C':
-                    score += 6
-            case 'Y':
-                score += 2
-                if opponent == 'B':
-                    score += 3
-                elif opponent == 'A':
-                    score += 6
-            case 'Z':
-                score += 3
-                if opponent == 'C':
-                    score += 3
-                elif opponent == 'B':
-                    score += 6
-            case _:
-                print('input invalid')
+                    if opponent == 'C':
+                        score += 3
+                    elif opponent == 'B':
+                        score += 6
+                case _:
+                    print('input invalid')
 
-    print(score)
-    input.close()
+        print(score)
+        input.close()
 
 
 if __name__ == '__main__':

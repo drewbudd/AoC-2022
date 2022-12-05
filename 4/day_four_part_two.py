5a85af307f51adce11e7f3141e8e6ece351d82cc
Andrew Almaguer <andrew.almaguer@jambit.com>
2022-12-05 09:04:34 +0100

    Solution for part two

--- 4/day_four_part_two.py
+++ 4/day_four_part_two.py
@@ -1,11 +1,11 @@
 '''Advent of Code 2022 Day Four'''
 
 def main():
-    '''https://adventofcode.com/2022/day/4'''
+    '''https://adventofcode.com/2022/day/4#part2'''
     with open('input.txt', 'r', encoding="utf8") as input_file:
         lines = input_file.readlines()
 
-        total_full_overlaps = 0
+        total_overlaps = 0
 
         for line in lines:
             assignment_one, assignment_two = line.strip().split(',')
@@ -15,10 +15,9 @@ def main():
             set_assignment_one = set(range(int(assignment_one[0]), int(assignment_one[1]) + 1))
             set_assignment_two = set(range(int(assignment_two[0]), int(assignment_two[1]) + 1))
 
-            if (set_assignment_one.issubset(set_assignment_two) 
-               or set_assignment_two.issubset(set_assignment_one)):
-                total_full_overlaps += 1        
-        print(f'Total full overlaps={total_full_overlaps}')
+            if len(set_assignment_one.intersection(set_assignment_two)) > 0:
+                total_overlaps += 1
+        print(f'Total overlaps={total_overlaps}')
 
 
 if __name__ == '__main__':
